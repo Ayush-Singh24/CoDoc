51ddf2a1631f58749a0ea6b1a448d8f39e1a6ba7
Ayush Singh <asayushsingh80@gmail.com>
2024-10-10 07:27:27 +0530

    feat:add livedocs provider

---
+++ src/app/Provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { ReactNode } from "react";
+
+export default function Provider({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+}
